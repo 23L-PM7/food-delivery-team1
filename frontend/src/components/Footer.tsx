b18d1c33106fadd948823e9ffab87f08723afa80
dmngo <164151923+DMNgoboki@users.noreply.github.com>
2024-03-21 16:12:28 +0800

    boldoo change

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,10 @@
 export function Footer() {
     return (
+        <>
         <div className="container mx-auto">
             <img src="/images/Footer.png" />      
         </div>
+        </>
+        
     );
 }
