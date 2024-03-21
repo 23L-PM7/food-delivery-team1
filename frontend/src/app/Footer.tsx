9b422fa8739495ff698249b9d9a7d79af3349553
dmngo <164151923+DMNgoboki@users.noreply.github.com>
2024-03-21 20:17:49 +0800

    up

--- frontend/src/app/Footer.tsx
+++ frontend/src/app/Footer.tsx
@@ -1,18 +1,24 @@
 import { BackgroundPic } from "./components/icons/backgroundPic";
-export function  Footer () {
-    return (
-        <div className="container mx-auto">
-           <div className="bg-green-500  w-[1440px] h-[545px] inset-0 relative">
-            <BackgroundPic/>
-            <div className="flex absolute">
-            <h1>Нүүр</h1>
-            <h1>Холбоо барих</h1>
-            <h1>Хоолны цэс</h1>
-            <h1>Үйлчилгээний нөхцөл</h1>
-            <h1>Хүргэлтийн бүс</h1>
-            <h1>Нууцлалын бодлого</h1>
+import { Logo } from "./components/icons/Logo";
+export function Footer() {
+  return (
+    <div className="container mx-auto">
+      <div className="bg-green-500  w-[1440px] h-[545px]  relative">
+        <div className=" absolute p-[120px]">
+            <div className="flex  justify-center">
+            <Logo/>
             </div>
-           </div>
+            <div className="flex">
+          <h1>Нүүр</h1>
+          <h1>Холбоо барих</h1>
+          <h1>Хоолны цэс</h1>
+          <h1>Үйлчилгээний нөхцөл</h1>
+          <h1>Хүргэлтийн бүс</h1>
+          <h1>Нууцлалын бодлого</h1>
+          </div>
         </div>
-    );
-} 
+        <BackgroundPic />
+      </div>
+    </div>
+  );
+}
