59be439e6cb87b2a9734c2e0b6eb404ceb54fd7d
dmngo <164151923+DMNgoboki@users.noreply.github.com>
2024-03-21 20:49:54 +0800

    up

--- frontend/src/app/Footer.tsx
+++ frontend/src/app/Footer.tsx
@@ -1,14 +1,17 @@
 import { BackgroundPic } from "./components/icons/backgroundPic";
 import { Logo } from "./components/icons/Logo";
+import { Facebook } from "./components/icons/Socials";
+import { Instagram } from "./components/icons/Socials";
+import { Twitter } from "./components/icons/Socials";
 export function Footer() {
   return (
     <div className="container mx-auto">
       <div className="bg-green-500  w-[1440px] h-[545px]  relative">
-        <div className=" absolute p-[120px]">
+        <div className="inset-0 absolute p-[120px] border-b">
             <div className="flex  justify-center">
             <Logo/>
             </div>
-            <div className="flex">
+            <div className="flex mt-[40px] justify-around text-white hover:underline">
           <h1>Нүүр</h1>
           <h1>Холбоо барих</h1>
           <h1>Хоолны цэс</h1>
@@ -16,6 +19,12 @@ export function Footer() {
           <h1>Хүргэлтийн бүс</h1>
           <h1>Нууцлалын бодлого</h1>
           </div>
+          <div className="flex justify-center gap-5 mt-[40px] border-b">
+          <Facebook/>
+          <Instagram/>
+          <Twitter/>
+          </div>
+          
         </div>
         <BackgroundPic />
       </div>
