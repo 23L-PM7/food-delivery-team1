73ce405fc04a1e3fee15c0436ff37c10a671ca47
dmngo <164151923+DMNgoboki@users.noreply.github.com>
2024-03-21 16:01:00 +0800

    up

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+    return (
+        <div className="container mx-auto">
+            <img src="/images/Footer.png" />      
+        </div>
+    );
+}
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,9 @@
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Footer } from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main></main>
+    <div>
+      <Footer/>
+    </div>
   );
 }
