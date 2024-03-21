a1f34254e9de451afaa033cf1aee57b7d6d4135d
dmngo <164151923+DMNgoboki@users.noreply.github.com>
2024-03-21 19:58:55 +0800

    up

--- frontend/src/app/Footer.tsx
+++ frontend/src/app/Footer.tsx
@@ -2,8 +2,16 @@ import { BackgroundPic } from "./components/icons/backgroundPic";
 export function  Footer () {
     return (
         <div className="container mx-auto">
-           <div className="bg-green-500  w-[1440px] h-[545px]">
+           <div className="bg-green-500  w-[1440px] h-[545px] inset-0 relative">
             <BackgroundPic/>
+            <div className="flex absolute">
+            <h1>Нүүр</h1>
+            <h1>Холбоо барих</h1>
+            <h1>Хоолны цэс</h1>
+            <h1>Үйлчилгээний нөхцөл</h1>
+            <h1>Хүргэлтийн бүс</h1>
+            <h1>Нууцлалын бодлого</h1>
+            </div>
            </div>
         </div>
     );
