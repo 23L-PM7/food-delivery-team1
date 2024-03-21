5dabd0861fceac1035fcfd5c8766567874aaa1c6
dmngo <164151923+DMNgoboki@users.noreply.github.com>
2024-03-21 19:50:36 +0800

    up

--- frontend/src/app/Footer.tsx
+++ frontend/src/app/Footer.tsx
@@ -1,7 +1,10 @@
+import { BackgroundPic } from "./components/icons/backgroundPic";
 export function  Footer () {
     return (
         <div className="container mx-auto">
-           <div className="bg-green-500 border-4 w-[1440px] h-[545px]"></div>
+           <div className="bg-green-500 border-4 w-[1440px] h-[545px]">
+            <BackgroundPic/>
+           </div>
         </div>
     );
 } 
