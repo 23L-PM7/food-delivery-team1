e7c5e3957227aa69ffd2ffaa96a2057c4de7bd83
Yesui <164152047+yesuia09@users.noreply.github.com>
2024-03-22 19:59:32 +0800

    neue

--- frontend/src/components/footerInfo.tsx
+++ frontend/src/components/footerInfo.tsx
@@ -8,27 +8,35 @@ export function Map() {
         <Pluslogo />
         <h1 className="font-extrabold">Хүргэлтийн бүс дэх хаягууд</h1>
       </div>
-      <div className="flex gap-5">
-        <div className="w-[588px] h-[388px] bg-white drop-shadow-lg rounded-lg text-black flex justify-evenly">
-          <div>
-            <h1>А бүс</h1>
-            <h1>Нархан хотхон</h1>
-            <h1>26-р байр</h1>
-            <h1>26-р байр</h1>
-            <h1>45-р байр</h1>
-            <h1>3-р байр</h1>
-            <h1>Хоймор хотхон</h1>
-            <h1>Хоймор хотхон</h1>
-          </div>
+      <div className="flex gap-5 m-10">
+        <div className="w-[588px] h-[388px] bg-white drop-shadow-lg rounded-lg text-black flex justify-center items-center">
+          <div className="h-[91.6%] w-11/12 flex flex-col">
+            {/* firsthalf */}
+            <div className="border">
+              <h1 className="font-semibold">A бүс</h1>
+            </div>
+            {/* second half */}
+            <div className="border flex justify-content">
+              <div className="w-1/2">
+                <h1>Нархан хотхон</h1>
+                <h1>26-р байр</h1>
+                <h1>26-р байр</h1>
+                <h1>45-р байр</h1>
+                <h1>3-р байр</h1>
+                <h1>Хоймор хотхон</h1>
+                <h1>Хоймор хотхон</h1>
+              </div>
 
-          <div>
-            <h1>Нархан хотхон</h1>
-            <h1>26-р байр</h1>
-            <h1>26-р байр</h1>
-            <h1>45-р байр</h1>
-            <h1>3-р байр</h1>
-            <h1>Хоймор хотхон</h1>
-            <h1>Хоймор хотхон</h1>
+              <div className="w-1/2">
+                <h1>Нархан хотхон</h1>
+                <h1>26-р байр</h1>
+                <h1>26-р байр</h1>
+                <h1>45-р байр</h1>
+                <h1>3-р байр</h1>
+                <h1>Хоймор хотхон</h1>
+                <h1>Хоймор хотхон</h1>
+              </div>
+            </div>
           </div>
         </div>
         <div className="w-[588px] h-[388px] bg-white drop-shadow-lg rounded-lg text-black ">
