f9117e69955462281e6fa43976ca0480361ea3a2
Yesui <164152047+yesuia09@users.noreply.github.com>
2024-03-25 18:11:49 +0800

    danke

--- frontend/src/app/deliveryzone/page.tsx
+++ frontend/src/app/deliveryzone/page.tsx
@@ -1,10 +1,11 @@
 import { Map } from "@/components/footerInfo";
 import { Terms } from "./terms";
-
+import { Privacy } from "./privacy";
 export default function DeliveryZone() {
   return (
     <div>
       <Terms />
+      <Privacy />
     </div>
   );
 }
