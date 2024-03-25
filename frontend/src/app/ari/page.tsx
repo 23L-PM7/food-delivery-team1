49fc6fa5b52363930088c00a9b9a481788bf9955
ashrv12 <bshirav@gmail.com>
2024-03-25 19:28:32 +0800

    update

--- frontend/src/app/ari/page.tsx
+++ frontend/src/app/ari/page.tsx
@@ -1,11 +1,19 @@
 import Signup from "@/components/Signup";
+import { Forgot } from "@/components/forgotpass/forgotpass";
 import { PopupSignin } from "@/components/popupSignin";
+import { Confirmation } from "@/components/forgotpass/sentConfirmation";
+import { NewPassword } from "@/components/forgotpass/newpassword";
+import { PassSetAlert } from "@/components/forgotpass/passwordAlert";
 
 export default function Home() {
   return (
     <div>
-      <Signup />
-      <PopupSignin />
+      {/* <Signup />
+      <PopupSignin /> */}
+      {/* <Forgot /> */}
+      {/* <Confirmation /> */}
+      {/* <NewPassword /> */}
+      <PassSetAlert />
     </div>
   );
 }
--- frontend/src/components/popupSignin.tsx
+++ frontend/src/components/popupSignin.tsx
@@ -1,8 +1,35 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Eye } from "./icons/eyeclosed";
 
 export function PopupSignin() {
   const [showModal, setShowModal] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [visible, setVisible] = useState("password");
+  const [check, setCheck] = useState("disabled");
+
+  //   change
+
+  const changePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const changeEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  //   visible
+
+  const visiblePassword = () => {
+    if (visible == "password") {
+      setVisible("text");
+    } else {
+      setVisible("password");
+    }
+  };
+
+  //   open close modals
 
   function openModal() {
     setShowModal(true);
@@ -12,16 +39,86 @@ export function PopupSignin() {
     setShowModal(false);
   }
 
+  //   check input fields useEffect
+
+  useEffect(() => {
+    checkForm();
+  });
+
+  const checkForm = () => {
+    if (
+      email === null ||
+      email === "" ||
+      password === null ||
+      password === ""
+    ) {
+      setCheck("disabled");
+    } else {
+      setCheck("");
+    }
+  };
+
   return (
     <div>
-      {/* Open the modal using document.getElementById('ID').showModal() method */}
-      <button className="btn" onClick={openModal}>
+      {/* <button className="btn" onClick={openModal}>
         open modal
-      </button>
+      </button> */}
+      {/* OPEN MODAL UGIIN AVTOMATAAR NEEGDEH YSTOI LOGIN HIIGEGUI BOL */}
       <dialog className={`modal ${showModal ? "modal-open" : ""}`}>
         <div className="modal-box">
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">Press ESC key or click outside to close</p>
+          <div className="flex flex-col gap-y-5">
+            <div className="w-full flex justify-center">
+              <h1 className="text-2xl font-bold">Бүртгүүлэх</h1>
+            </div>
+            <div className="flex flex-col gap-y-3">
+              {/* email */}
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text">Имэйл</span>
+                </div>
+                <input
+                  type="text"
+                  value={email}
+                  onChange={changeEmail}
+                  placeholder="Имэйл хаягаа оруулна уу"
+                  className="input bg-gray-100 rounded-none w-full border border-gray-200"
+                />
+              </label>
+              {/* password */}
+              <label className="form-control w-full">
+                <div className="label">
+                  <span className="label-text">Нууц үг</span>
+                </div>
+                <label className="input flex items-center justify-between bg-gray-100 rounded-none border border-gray-200">
+                  <input
+                    type={visible}
+                    value={password}
+                    onChange={changePassword}
+                    placeholder="Нууц үгээ оруулна уу"
+                  />
+                  <button onClick={visiblePassword}>
+                    <Eye />
+                  </button>
+                </label>
+              </label>
+              {/* button for forgot password */}
+              <div className="w-full flex justify-end">
+                <button>Нууц үг сэргээх</button>
+              </div>
+            </div>
+            {/* first div end */}
+            <div className="flex flex-col gap-y-5">
+              <button
+                className="btn bg-green-500 text-white max-w-none w-full hover:bg-green-500"
+                disabled={check}
+              >
+                Нэвтрэх
+              </button>
+              <button className="btn btn-outline outline-green-500 border-green-500 hover:bg-green-500 hover:border-green-500">
+                Бүртгүүлэх
+              </button>
+            </div>
+          </div>
         </div>
         <form method="dialog" className="modal-backdrop">
           <button onClick={closeModal}>close</button>
