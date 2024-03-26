539d58a8fee4d794f0a4dc9b6d059253ad4aff76
Buynaa17 <23lp8156@nest.edu.mn>
2024-03-26 20:20:41 +0800

    .
    .

--- frontend/src/components/orderconfirmation.tsx
+++ frontend/src/components/orderconfirmation.tsx
@@ -1,8 +1,10 @@
+import { Span } from "next/dist/trace";
+
 function OrderCon() {
   return (
     <div className="flex justify-center">
       <main className=" block rounded-xl">
-        <div className="flex gap-2">
+        <div className="flex gap-2 px-4 py-6">
           <svg
             width="48"
             height="48"
@@ -20,7 +22,7 @@ function OrderCon() {
           </div>
         </div>
         <main
-          className="w-[432px] h-[612px] block border-2
+          className="w-[432px] h-[612px] block border-2 gap-4 p-6
         "
         >
           Хаяг аа оруулна уу
@@ -94,7 +96,7 @@ function OrderCon() {
         </main>
       </main>
       <main className="block rounded-xl">
-        <div className="flex gap-2">
+        <div className="flex gap-2 px-4 py-6">
           <svg
             width="48"
             height="48"
@@ -111,7 +113,36 @@ function OrderCon() {
             <h5>Хүлээгдэж байна</h5>
           </div>
         </div>
-        <main className="w-[432px] h-[612px]  border-2"></main>
+        <main className="w-[432px] h-[612px]  border-2 ">
+          <div className="flex gap-2 p-6">
+            <img
+              src="/images/Image.png"
+              className="w-[184px] h-[121px] object-cover"
+            />
+
+            <div>
+              <h1 className="font-semibold text-lg">Main Pizza </h1>
+              <h2
+                className="font-semibold text-lg text-green-600 mb-2
+            "
+              >
+                34,800₮
+              </h2>
+              <h4 className="text-neutral-500">
+                Хулуу, төмс, лууван , сонгино, цөцгийн тос, самрын үр{" "}
+              </h4>{" "}
+            </div>
+          </div>
+          <main className="flex gap-3 w-[384px] h-[54px]">
+            <span>
+              <h1>Нийт төлөх дүн</h1>
+              <h1>34,800₮</h1>
+            </span>
+            <span className="px-2 py-3 w-[187px] h-[47px] bg-[#EEEFF2]">
+              Захиалах
+            </span>{" "}
+          </main>
+        </main>
       </main>
     </div>
   );
