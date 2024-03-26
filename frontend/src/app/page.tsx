263f4c800318961bfb342b58623935e7ece32eee
dmngo <164151923+DMNgoboki@users.noreply.github.com>
2024-03-26 14:59:10 +0800

    up

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,4 @@
-<<<<<<< Updated upstream
+
 import { Footer } from "./Footer";
 import Header from "../components/Header";
 import { Login } from "@/components/Login";
@@ -9,20 +9,9 @@ export default function Home() {
   return (
     <div>
        <Header />
-      
        <Login/>
        <UserProfile/>
       <Footer />
     </div>
-=======
-import {Mainbutton} from "@/app/components/Mainbutton"
-
-export default function Home() {
-  return (
-    <>
-      <Mainbutton/>
-
-    </>
->>>>>>> Stashed changes
   );
 }
