69f1228f8d99e02286ef4dbded5aa83f3ace7553
ashrv12 <bshirav@gmail.com>
2024-03-26 17:40:11 +0800

    Mnde's order detail

--- frontend/src/components/category.tsx
+++ frontend/src/components/category.tsx
@@ -1,19 +1,20 @@
 export function Category() {
   return (
-    <div className="container px-[120px] py-[32px]">
-    <div className="flex gap-[26px]">
+    <div className="w-full px-[120px] py-[32px]">
+      <div className="w-full flex justify-between gap-[26px]">
         <button className="btn w-[280px] h-[43px] outline-slate-200 text-black hover:bg-green-500 hover:text-white bg-transparent">
-        Breakfast
-      </button>
-      <button className="btn w-[280px] h-[43px] outline-slate-200 text-black hover:bg-green-500 hover:text-white bg-transparent">
-      Soup</button>
-      <button className="btn w-[280px] h-[43px] outline-slate-200 text-black hover:bg-green-500 hover:text-white bg-transparent">
-        Main course
-      </button>
-      <button className="btn w-[280px] h-[43px] outline-slate-200 text-black hover:bg-green-500 hover:text-white bg-transparent">
-        Dessert
-      </button>
-    </div>
+          Breakfast
+        </button>
+        <button className="btn w-[280px] h-[43px] outline-slate-200 text-black hover:bg-green-500 hover:text-white bg-transparent">
+          Soup
+        </button>
+        <button className="btn w-[280px] h-[43px] outline-slate-200 text-black hover:bg-green-500 hover:text-white bg-transparent">
+          Main course
+        </button>
+        <button className="btn w-[280px] h-[43px] outline-slate-200 text-black hover:bg-green-500 hover:text-white bg-transparent">
+          Dessert
+        </button>
+      </div>
     </div>
   );
 }
