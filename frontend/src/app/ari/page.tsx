5d138d05da4cabef39e7994d132399c3283e0177
ashrv12 <bshirav@gmail.com>
2024-03-26 18:22:42 +0800

    update

--- frontend/src/app/ari/page.tsx
+++ frontend/src/app/ari/page.tsx
@@ -5,10 +5,12 @@ import { Confirmation } from "@/components/forgotpass/sentConfirmation";
 import { NewPassword } from "@/components/forgotpass/newpassword";
 import { PassSetAlert } from "@/components/forgotpass/passwordAlert";
 import { AdminDashboard } from "@/components/adminComp/adminDashboard";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <div>
+      <Header />
       {/* <Signup />
       <PopupSignin /> */}
       {/* <Forgot /> */}
