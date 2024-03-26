bf36c8b33fb983fa1d711c6cd6b7d1f2309c3ab3
Yesui <164152047+yesuia09@users.noreply.github.com>
2024-03-26 18:38:10 +0800

    biip

--- frontend/src/app/yesui/page.tsx
+++ frontend/src/app/yesui/page.tsx
@@ -1,11 +1,13 @@
 import { No } from "./errors/no";
 import { Cap } from "./errors/cap";
+import { Ded } from "./errors/ded";
 
 export default function Yesui() {
   return (
     <div>
-      <Cap />
-      <No />
+      <Ded />
+      {/* <Cap />
+      <No /> */}
     </div>
   );
 }
