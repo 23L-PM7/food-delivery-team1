48f44934cefe7e8e8a46b81242404caac18688f1
MNDE25 <151522647+MNDE25@users.noreply.github.com>
2024-03-27 17:45:50 +0800

    mnde_card

--- frontend/src/app/mnde/menu.tsx
+++ frontend/src/app/mnde/menu.tsx
@@ -2,20 +2,10 @@ import Card from "./card";
 
 export default function Menu() {
     return (
-        <div className="container mx-auto px-[120px] mt-[54px]">
+        <div className="container mx-auto px-[120px] mt-[54px] mb-[86px]">
             <div className="grid grid-rows-3 grid-cols-4 gap-x-[24px] gap-y-[60px]">
                 <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                
             </div>
         </div>
 
