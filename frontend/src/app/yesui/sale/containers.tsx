39da3a40c660063723878b79ed6d2085ef91d5da
Yesui <164152047+yesuia09@users.noreply.github.com>
2024-03-27 20:04:57 +0800

    updown

--- frontend/src/app/yesui/sale/containers.tsx
+++ frontend/src/app/yesui/sale/containers.tsx
@@ -1,11 +1,11 @@
 export function Containers() {
   return (
-    <div className="w-[587px] h-[702px] m-5">
-      <div className="flex justify-center border-b border-gray-400">
-        <div className="w-[10%] flex items-center text-2xl">
+    <div className="w-[587px] h-[702px] m-5 border rounded-2xl">
+      <div className="flex h-[64px] justify-center border-b border-gray-400">
+        <div className="w-[10%] flex items-center justify-center text-2xl border">
           <h1 className="font-sm">x</h1>
         </div>
-        <div className="w-[80%] flex justify-center">
+        <div className="w-[80%] flex justify-center items-center">
           <h1 className="font-bold text-2xl">Create food</h1>
         </div>
         {/* just for centering the main title CREATE FOOD */}
@@ -19,25 +19,25 @@ export function Containers() {
         <input
           type="text"
           placeholder="Placeholder"
-          className="input border-none input-bordered input-lg w-full max-w-xxl bg-gray-200"
+          className="input h-[56px] border-none input-bordered input-lg w-full max-w-xxl bg-gray-200"
         />
         <h1>Хоолны ангилал</h1>
         <input
           type="text"
           placeholder="Placeholder"
-          className="input border-none input-bordered input-lg w-full max-w-xxl bg-gray-200"
+          className="input h-[56px] border-none input-bordered input-lg w-full max-w-xxl bg-gray-200"
         />
         <h1>Хоолны орц</h1>
         <input
           type="text"
           placeholder="Placeholder"
-          className="input border-none input-bordered input-lg w-full max-w-xxl bg-gray-200"
+          className="input h-[56px] border-none input-bordered input-lg w-full max-w-xxl bg-gray-200"
         />
         <h1>Хоолны үнэ</h1>
         <input
           type="text"
           placeholder="Placeholder"
-          className="input border-none input-bordered input-lg w-full max-w-xxl bg-gray-200"
+          className="input h-[56px] border-none input-bordered input-lg w-full max-w-xxl bg-gray-200"
         />
       </div>
     </div>
