e539527c86b27b8192a0934faf095944eaae8cf4
dmngo <164151923+DMNgoboki@users.noreply.github.com>
2024-03-28 18:42:13 +0800

    ss

--- frontend/src/components/UserProfile.tsx
+++ frontend/src/components/UserProfile.tsx
@@ -1,11 +1,24 @@
 "use client";
 
+import { useState } from "react";
 import { Call, Exit, Mail, Pencil, Person, Timer } from "./icons/ProfileIcons";
 function myFunction() {
   document.getElementById("myDialog").showModal();
 }
 
+const userpro = {
+  name: "УгтахБаяр",
+  number: "88883345",
+  email: "Ugtakhbayr@gmail.com",
+  image: "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg",
+};
+
 export function UserProfile() {
+  const [userpro, setUserpro] = useState("");
+
+
+
+
   return (
     <div className="container mx-auto  w-[432px] px-[20px] mb-[40px]">
       <div className="">
@@ -30,7 +43,7 @@ export function UserProfile() {
           </div>
           <div className="w-8/12">
             <h1 className="text-xs text-[#888A99]">Таны нэр</h1>
-            <h1 className="text-base">УгтахБаяр</h1>
+            <h1 className="text-base">{userpro}</h1>
           </div>
           <div className="w-2/12 justify-end flex">
             <Pencil />
