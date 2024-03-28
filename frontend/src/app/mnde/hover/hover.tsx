21d3a3e0b82a7a2461d9a1792201d48dbfecba49
MNDE25 <151522647+MNDE25@users.noreply.github.com>
2024-03-28 19:43:20 +0800

    onClick hiine

---
+++ frontend/src/app/mnde/hover/hover.tsx
@@ -0,0 +1,10 @@
+export default function hover() {
+    return (
+        <div id="modal" className="hidden fixed inset-[0] justify-center items-center" >
+            <div className="absolute inset-[0] bg-[#000000]/50">
+            </div>
+            <div className="relative w-[981px] h-[564px] bg-white items-center p-[32px] rounded-[16px]">
+            </div>
+        </div>
+        )
+}
