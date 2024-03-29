dc9ca032e6a75ea57e0386694c8c3e6c34740698
ashrv12 <bshirav@gmail.com>
2024-03-29 19:42:22 +0800

    qaa

--- frontend/src/components/adminComp/adminCategory.tsx
+++ frontend/src/components/adminComp/adminCategory.tsx
@@ -9,6 +9,7 @@ import {
 
 export function AdminCategory() {
   const [color, setColor] = useState("Black");
+  const [openModal, setOpenModal] = useState("");
 
   const category = [
     {
@@ -23,6 +24,14 @@ export function AdminCategory() {
     { name: "Dessert", id: 126 },
   ];
 
+  const openCategoryModal = () => {
+    if (openModal == "") {
+      setOpenModal("modal-open");
+    } else {
+      setOpenModal("");
+    }
+  };
+
   return (
     <div className="bg-gray-100">
       <div className="w-full container flex mx-auto">
@@ -34,15 +43,31 @@ export function AdminCategory() {
           </div>
           {/* category mapped */}
           <div className="w-[258px] flex flex-col gap-y-5">
-            {category.map((item) => (
-              <div className="h-[40px] active:bg-green-500 active:text-white border rounded-md flex items-center px-3 justify-between drop-shadow-md">
+            {category.map((item, index) => (
+              <div
+                style={{ zIndex: 100 - index }}
+                className="h-[40px] relative active:bg-green-500 active:text-white border rounded-md flex items-center px-3 justify-between drop-shadow-md"
+              >
                 <h3 className="text-lg font-medium">{item.name}</h3>
-                <button>
-                  <TripleDotBlack />
-                </button>
+                <div className="dropdown dropdown-end z-10">
+                  <button tabIndex={0} role="button" className="m-1">
+                    <TripleDotBlack />
+                  </button>
+                  <ul
+                    tabIndex={0}
+                    className="absolute dropdown-content z-10 menu p-2 shadow bg-white rounded-box w-52"
+                  >
+                    <li>
+                      <a>Edit</a>
+                    </li>
+                    <li>
+                      <a>Delete</a>
+                    </li>
+                  </ul>
+                </div>
               </div>
             ))}
-            <button>
+            <button onClick={openCategoryModal}>
               <div className="btn btn-sm bg-white h-[40px] flex">
                 <GreyAdd />
                 <h1 className="text-gray-500">Create new Category</h1>
@@ -62,6 +87,42 @@ export function AdminCategory() {
           {/* food content */}
         </div>
       </div>
+      {/* modal */}
+      <dialog id="create_category" className={`modal ${openModal}`}>
+        <div className="modal-box p-0">
+          <div className="flex w-full justify-between items-center p-4 border-b-[1px]">
+            <h1 className="text-xl font-bold">X</h1>
+            <h1 className="text-2xl font-semibold">Create New Category</h1>
+            <h1 className="text-xl font-bold text-white">x</h1>
+          </div>
+          <div className="flex flex-col p-4">
+            <label className="form-control w-full">
+              <div className="label">
+                <span className="label-text">Category Name</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Category Name"
+                className="input input-bordered w-full bg-gray-200"
+              />
+            </label>
+          </div>
+          <div className="flex justify-end p-4 gap-3">
+            <button
+              onClick={openCategoryModal}
+              className="btn btn-sm h-[40px] btn-outline"
+            >
+              Cancel
+            </button>
+            <button className="btn btn-sm h-[40px] btn-neutral text-white">
+              Continue
+            </button>
+          </div>
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button onClick={openCategoryModal}>close</button>
+        </form>
+      </dialog>
     </div>
   );
 }
