036120020c0948128f11e5b72af7db8f532909e3
MNDE25 <151522647+MNDE25@users.noreply.github.com>
2024-03-29 18:46:27 +0800

    modal1 done garj iruulj chadadguie thde hha

--- frontend/src/app/mnde/hover/modal.tsx
+++ frontend/src/app/mnde/hover/modal.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 
-export default function Hover() {
+export default function Modal() {
   const [foodQuantity, setFoodquantity] = useState(1);
 
   const incementFoodQuantity = () => {
@@ -15,12 +15,12 @@ export default function Hover() {
   return (
     <div
       id="modal"
-      className=" fixed inset-[0] flex justify-center items-center"
+      className=" fixed inset-[0] flex justify-center items-center "
     >
       <div className="absolute inset-[0] bg-[#000000]/50"></div>
       <div className="relative w-[981px] h-[564px] bg-white m-auto p-[32px] rounded-[16px]">
         <svg
-          className="absolute top-2 right-2 fill-current"
+          className="absolute top-[37px] right-[37px] fill-current"
           xmlns="http://www.w3.org/2000/svg"
           width="32"
           height="32"
@@ -28,15 +28,15 @@ export default function Hover() {
         >
           <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
         </svg>
-        <div className="flex gap-[33px]">
+        <div className="container flex flex-row justify-between gap-[33px]">
           <div className="w-[500px] h-[500px]">
             <img
-              className="object-fill"
+              className="object-cover w-[500px] h-[500px]"
               src="./images/menu food photos/menu-1.png"
             ></img>
           </div>
-          <div className="flex flex-col gap-[32px]">
-            <div className="flex flex-col gap-[2px]">
+          <div className="flex flex-col gap-[32px] justify-center">
+            <div className="flex flex-col gap-[2px] w-[384px]">
               <div className="font-semibold text-lg text-[#000000]">
                 food name
               </div>
@@ -51,18 +51,24 @@ export default function Hover() {
               </div>
             </div>
             <div className="font-semibold text-lg text-[#000000]">Тоо</div>
-            <div className="flex">
-              <button className="btn" onClick={incementFoodQuantity}>
-                {" "}
-                +{" "}
-              </button>
-              <div>{foodQuantity}</div>
-              <button className="btn" onClick={minusFoodQuantity}>
+            <div className="flex justify-between">
+              <button
+                className="btn bg-[#18BA51] text-[#FFFFFF]"
+                onClick={minusFoodQuantity}
+              >
                 {" "}
                 -{" "}
               </button>
+              <div>{foodQuantity}</div>
+              <button
+                className="btn bg-[#18BA51] text-[#FFFFFF]"
+                onClick={incementFoodQuantity}
+              >
+                {" "}
+                +{" "}
+              </button>
             </div>
-            <button className="btn">Сагслах</button>
+            <button className="btn bg-[#18BA51] text-[#FFFFFF]">Сагслах</button>
           </div>
         </div>
       </div>
--- frontend/src/app/mnde/menu.tsx
+++ frontend/src/app/mnde/menu.tsx
@@ -1,36 +1,36 @@
+"use client";
 import { foodlist } from "./datalist";
-import Hover from "./hover/hover";
+import Modal from "./hover/modal";
 
 export default function Menu() {
-    return (
-        <div className="container mx-auto px-[120px] mt-[54px] mb-[86px]">
-            <div className="grid grid-rows-3 grid-cols-4 gap-x-[24px] gap-y-[60px]">
-                {foodlist.map((food) => (
-                    <div>
-                        <div className="relative overflow-hidden w-fit" >
-                            <div className="absolute 20% bg-green-600 text-white w-[69px] h-[35px] left-[197px] top-[16px] rounded-[16px] border-[1px] flex item-center justify-center">20%
-                            </div>
-                            <img src={food.img}></img>
-                            <div className="absolute h-full w-full bg-black/50 bottom-0 right-0 flex items-center justify-center opacity-0 hover:opacity-100 rounded-2xl" >
-                                <button className="bg-white text-black p-[8px] w-[166px] h-[40px] rounded-[100px]">Edit</button>
-                            </div>
-                        </div>
-                        <div className="flex flex-col mt-[14px]">
-                            <div className="font-semibold text-lg text-[#000000]">
-                                {food.tittle}
-                            </div>
-                            <div className="font-semibold text-lg text-[#18BA51] mt-[2px]">
-                                {food.price}₮
-                            </div>
-                        </div>
-                    </div>
-                ))}
-                <div>
-                    <Hover />
-                </div>
+  return (
+    <div className="container mx-auto px-[120px] mt-[54px] mb-[86px]">
+      <div className="grid grid-rows-3 grid-cols-4 gap-x-[24px] gap-y-[60px]">
+        {foodlist.map((food) => (
+          <div>
+            <div className="relative overflow-hidden w-fit">
+              <div className="absolute 20% bg-green-600 text-white w-[69px] h-[35px] left-[197px] top-[16px] rounded-[16px] border-[1px] flex item-center justify-center">
+                20%
+              </div>
+              <img src={food.img}></img>
+              <div className="absolute h-full w-full bg-black/50 bottom-0 right-0 flex items-center justify-center opacity-0 hover:opacity-100 rounded-2xl">
+                <button className="bg-white text-black p-[8px] w-[166px] h-[40px] rounded-[100px]">
+                  Edit
+                </button>
+              </div>
             </div>
-        </div>
-
-
-    )
+            <div className="flex flex-col mt-[14px]">
+              <div className="font-semibold text-lg text-[#000000]">
+                {food.tittle}
+              </div>
+              <div className="font-semibold text-lg text-[#18BA51] mt-[2px]">
+                {food.price}₮
+              </div>
+            </div>
+          </div>
+        ))}
+        <Modal />
+      </div>
+    </div>
+  );
 }
