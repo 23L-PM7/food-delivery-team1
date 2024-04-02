aeb93d82a58ac80ffbe1ed9d57eeb7cadf415e06
ashrv12 <bshirav@gmail.com>
2024-04-02 18:52:16 +0800

    update

--- frontend/src/components/adminComp/adminCategorySelect.tsx
+++ frontend/src/components/adminComp/adminCategorySelect.tsx
@@ -9,63 +9,115 @@ import {
 import { Pen } from "../icons/pen";
 import { Trash } from "../icons/trash";
 
-export function SideCategory() {
-  const [selected, setSelected] = useState("Breakfast");
+type ChildProps = {
+  zindex: number;
+  label: string;
+  id: number;
+};
 
-  const category = [
-    {
-      name: "Breakfast",
-      id: 123,
-    },
-    { name: "Lunch", id: 124 },
-    {
-      name: "Dinner",
-      id: 125,
-    },
-    { name: "Dessert", id: 126 },
-  ];
+// id: number, label: string
 
-  const selectCategory = (name: string) => {
-    setSelected(name);
+export function SideCategory(props: ChildProps) {
+  const { zindex, label, id } = props;
+  const [selected, setSelected] = useState(false);
+  const [openEdit, setEdit] = useState(false);
+
+  const openEditModal = () => {
+    setEdit(!openEdit);
   };
+
+  const selectCategory = () => {
+    setSelected(!selected);
+  };
+
+  const clearField = () => {};
 
   return (
     <>
-      {category.map((item, index) => (
-        <div
-          style={{
-            zIndex: 100 - index,
-            backgroundColor: selected === item.name ? "#18BA51" : "",
-            color: selected === item.name ? "white" : "",
-          }}
-          className={`h-[40px] relative border rounded-xl flex items-center px-3 justify-between drop-shadow-md`}
-          onClick={() => selectCategory(item.name)}
+      <div
+        style={{
+          zIndex: zindex,
+          backgroundColor: selected ? "#18BA51" : "",
+          color: selected ? "white" : "",
+        }}
+        className={`h-[40px] relative border rounded-xl flex items-center px-3 justify-between drop-shadow-md`}
+      >
+        <h3
+          onClick={() => selectCategory()}
+          className="cursor-pointer text-lg font-medium"
         >
-          <h3 className="text-lg font-medium">{item.name}</h3>
-          <div className="dropdown dropdown-end z-10">
-            <button tabIndex={0} role="button" className="m-1">
-              {selected === item.name ? <TripleDotWhite /> : <TripleDotBlack />}
+          {label}
+        </h3>
+        <div className="dropdown dropdown-end z-10">
+          <button tabIndex={0} role="button" className="m-1">
+            {selected ? <TripleDotWhite /> : <TripleDotBlack />}
+          </button>
+          <ul
+            tabIndex={0}
+            className="absolute dropdown-content z-10 menu p-2 shadow bg-white rounded-md w-52 text-black"
+          >
+            <li>
+              <button
+                className="flex justify-start"
+                onClick={() => openEditModal()}
+              >
+                <Pen />
+                Edit name
+              </button>
+            </li>
+            <li>
+              <button className="text-red-500 flex justify-start">
+                <Trash />
+                Delete category
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+      {/* modal create */}
+
+      {/* modal edit */}
+      <dialog
+        id="create_category"
+        className={`modal ${openEdit ? "modal-open" : ""}`}
+      >
+        <div className="modal-box p-0">
+          <div className="flex w-full justify-between items-center p-4 border-b-[1px]">
+            <button onClick={openEditModal} className="text-xl font-bold">
+              X
             </button>
-            <ul
-              tabIndex={0}
-              className="absolute dropdown-content z-10 menu p-2 shadow bg-white rounded-md w-52 text-black"
+            <h1 className="text-2xl font-semibold">Edit Category</h1>
+
+            <h1 className="text-xl font-bold text-white">x</h1>
+          </div>
+          <div className="flex flex-col p-4">
+            <label className="form-control w-full">
+              <div className="label">
+                <span className="label-text">Category Name</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Category Name"
+                className="input input-bordered w-full bg-gray-200"
+              />
+            </label>
+          </div>
+          <div className="flex justify-end p-4 gap-3">
+            <button
+              onClick={clearField}
+              className="btn btn-sm h-[40px] btn-outline"
             >
-              <li>
-                <button className="flex justify-start">
-                  <Pen />
-                  Edit name
-                </button>
-              </li>
-              <li>
-                <button className="text-red-500 flex justify-start">
-                  <Trash />
-                  Delete category
-                </button>
-              </li>
-            </ul>
+              Clear
+            </button>
+            <button className="btn btn-sm h-[40px] btn-neutral text-white">
+              Continue
+            </button>
           </div>
         </div>
-      ))}
+        <form method="dialog" className="modal-backdrop">
+          <button onClick={openEditModal}>close</button>
+        </form>
+      </dialog>
     </>
   );
 }
