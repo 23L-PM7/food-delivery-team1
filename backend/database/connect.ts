883e48c36a7a1efb98f0edd2789de0ddb1cbe365
Buynaa17 <23lp8156@nest.edu.mn>
2024-04-02 20:55:03 +0800

    .
    .

--- backend/database/connect.ts
+++ backend/database/connect.ts
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
-import 'dotenv/config'
+import "dotenv/config";
 
-
-const connectionString = process.env.MONGO_URL || ''
+const connectionString = process.env.MONGO_URL || "";
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect(connectionString);
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.log("Failed to connect to MongoDB", error);
-    }
+  try {
+    await mongoose.connect(connectionString);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Failed to connect to MongoDB", error);
+  }
 };
