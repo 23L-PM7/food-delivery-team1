31eb01939abb0ab61d97156c2b7d3c6b108f141a
Buynaa17 <23lp8156@nest.edu.mn>
2024-04-03 19:41:17 +0800

    .
    .

--- backend/controller/users.controller.ts
+++ backend/controller/users.controller.ts
@@ -1,47 +1,42 @@
 import { UsersModel } from "../models/user.model";
 
-
-
 export async function getUsers(req: any, res: any) {
-    const users = await UsersModel.find();
+  const users = await UsersModel.find();
 
-    res.json(users);
+  res.json(users);
 }
 
 export async function createUsers(req: any, res: any) {
-    const { id, name, email, password, phoneNumber, role } = req.body;
-    console.log(req.body);
+  const { id, name, email, password, phoneNumber, role } = req.body;
+  console.log(req.body);
 
-    const users = await UsersModel.create({
-        id: id,
-        name: name,
-        email: email,
-        password: password,
-        phoneNumber: phoneNumber,
-        role: role,
-    });
-    res.json(users);
+  const users = await UsersModel.create({
+    id: id,
+    name: name,
+    email: email,
+    password: password,
+    phoneNumber: phoneNumber,
+    role: role,
+  });
+  res.json(users);
 }
-
-
-
 
 export async function updateUsers(req: any, res: any) {
-    const { name, email, password, phoneNumber, role } = req.body;
-    const { id } = req.params;
+  const { name, email, password, phoneNumber, role } = req.body;
+  const { id } = req.params;
 
-    const users = await UsersModel.findByIdAndUpdate(id, {
-        name: name,
-        email: email,
-        password: password,
-        phoneNumber: phoneNumber,
-        role: role,
-    });
-    res.json(users);
+  const users = await UsersModel.findByIdAndUpdate(id, {
+    name: name,
+    email: email,
+    password: password,
+    phoneNumber: phoneNumber,
+    role: role,
+  });
+  res.json(users);
 }
 export async function deleteUsers(req: any, res: any) {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    const users = await UsersModel.findByIdAndDelete(id);
-    res.json(users);
+  const users = await UsersModel.findByIdAndDelete(id);
+  res.json(users);
 }
