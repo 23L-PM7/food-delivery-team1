9f0e777c77b32f97293fb3534d8be4653347a238
Yesui <164152047+yesuia09@users.noreply.github.com>
2024-04-03 18:26:18 +0800

    up

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import { Mainbutton } from "@/components/herobutton/Mainbutton";
 import Menu from "./mnde/menu";
 import { Desktop } from "@/components/Desktop";
+import { Nogoonplus } from "@/components/icons/nogoonplus";
 
 export default function Home() {
   return (
