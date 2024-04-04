01ba3c59f64fe2f3129a0b06311e5cda424c9dfe
ashrv12 <bshirav@gmail.com>
2024-04-04 17:33:53 +0800

    update

--- backend/controller/users.controller.ts
+++ backend/controller/users.controller.ts
@@ -1,82 +1,82 @@
 import { UsersModel } from "../models/user.model";
+import jwt from "jsonwebtoken";
 
 export async function getUsers(req: any, res: any) {
-    const users = await UsersModel.find();
+  const users = await UsersModel.find();
 
-    res.json(users);
+  res.json(users);
 }
 
 export async function createUsers(req: any, res: any) {
-    const { id, name, email, password, phoneNumber, role } = req.body;
-    console.log(req.body);
+  const { id, name, email, password, phoneNumber, role } = req.body;
+  console.log(req.body);
 
-    const users = await UsersModel.create({
-        id: id,
-        name: name,
-        email: email,
-        password: password,
-        phoneNumber: phoneNumber,
-        role: role,
-    });
-    res.json(users);
+  const users = await UsersModel.create({
+    id: id,
+    name: name,
+    email: email,
+    password: password,
+    phoneNumber: phoneNumber,
+    role: role,
+  });
+  res.json(users);
 }
 
 export async function updateUsers(req: any, res: any) {
-    const { name, email, password, phoneNumber, role } = req.body;
-    const { id } = req.params;
+  const { name, email, password, phoneNumber, role } = req.body;
+  const { id } = req.params;
 
-    const users = await UsersModel.findByIdAndUpdate(id, {
-        name: name,
-        email: email,
-        password: password,
-        phoneNumber: phoneNumber,
-        role: role,
-    });
-    res.json(users);
+  const users = await UsersModel.findByIdAndUpdate(id, {
+    name: name,
+    email: email,
+    password: password,
+    phoneNumber: phoneNumber,
+    role: role,
+  });
+  res.json(users);
 }
 export async function deleteUsers(req: any, res: any) {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    const users = await UsersModel.findByIdAndDelete(id);
-    res.json(users);
+  const users = await UsersModel.findByIdAndDelete(id);
+  res.json(users);
 }
 
 ////////////////////LOGIN HESEG////////////////////
 
-
 export async function getLogin(req: any, res: any) {
-    res.json(["dorj", "dolgor"]);
+  res.json(["dorj", "dolgor"]);
 }
 
 export async function createLogin(req: any, res: any) {
-    const { email, password } = req.body;
-    console.log(req.body);
+  const { email, password } = req.body;
+  console.log(req.body);
 
-    UsersModel.findOne({ email: "bat@gmail.com" });
-    const loggedin = true;
+  UsersModel.findOne({ email: "bat@gmail.com" });
+  const loggedin = true;
 
-    if (loggedin) {
-        const access = jwt.sign({ email: email, password: password }, "dd");
-        res.json({ access });
-    }
+  if (loggedin) {
+    const access = jwt.sign({ email: email, password: password }, "dd");
+    res.json({ access });
+  }
 
-    res.sendStatus(401);
+  res.sendStatus(401);
 }
 
 export async function updateLogin(req: any, res: any) {
-    const { email, password } = req.body;
-    const { id } = req.params;
+  const { email, password } = req.body;
+  const { id } = req.params;
 
-    const usersLogin = await UsersModel.findByIdAndUpdate(id, {
-        email: email,
-        password: password,
-    });
-    res.json(usersLogin);
+  const usersLogin = await UsersModel.findByIdAndUpdate(id, {
+    email: email,
+    password: password,
+  });
+  res.json(usersLogin);
 }
 export async function deleteLogin(req: any, res: any) {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    const usersLogin = await UsersModel.findByIdAndDelete(id);
+  const usersLogin = await UsersModel.findByIdAndDelete(id);
 
-    res.json(usersLogin);
+  res.json(usersLogin);
 }
