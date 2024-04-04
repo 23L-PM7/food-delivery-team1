2033ac0d191ecbb8592143888e198d5e7477c535
ashrv12 <bshirav@gmail.com>
2024-04-04 18:46:11 +0800

    category update

--- backend/index.ts
+++ backend/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { connectDB } from "./database/connect";
 import usersRouter from "./routers/usersRouter";
+import categoryRouter from "./routers/categoryRouter";
 
 const app = express();
 const port = 9090;
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/users", usersRouter);
+app.use("/category", categoryRouter);
 
 connectDB();
 
