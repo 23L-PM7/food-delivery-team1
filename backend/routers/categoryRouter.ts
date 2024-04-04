fdf4386b813dfe0607235dff61f69cfd5336a5d9
ashrv12 <bshirav@gmail.com>
2024-04-04 18:37:11 +0800

    fixed router

--- backend/routers/categoryRouter.ts
+++ backend/routers/categoryRouter.ts
@@ -11,7 +11,7 @@ const categoryRouter = express.Router();
 
 categoryRouter.get("/", getCategory);
 categoryRouter.post("/create", createCategory);
-categoryRouter.put("/update/[id]", updateCategory);
-categoryRouter.delete("/delete/[id]", deleteCategory);
+categoryRouter.put("/update/:id", updateCategory);
+categoryRouter.delete("/delete/:id", deleteCategory);
 
 export default categoryRouter;
