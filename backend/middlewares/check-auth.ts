0d485bcf3275a687c35a465b8e9ef1aca03e41b6
Buynaa17 <23lp8156@nest.edu.mn>
2024-04-09 16:29:07 +0800

    .
    .

--- backend/middlewares/check-auth.ts
+++ backend/middlewares/check-auth.ts
@@ -2,18 +2,17 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
 export function checkAuth(req: Request, res: Response, next: NextFunction) {
-    const accessToken = req.get("access-token");
+  const accessToken = req.get("access-token");
 
+  if (!accessToken) {
+    return res.sendStatus(403);
+  }
 
-    if (!accessToken) {
-        return res.sendStatus(403);
-    }
-
-    try {
-        const decoded = jwt.verify(accessToken, "dmngo");
-        console.log({ decoded });
-        next();
-    } catch (error) {
-        return res.sendStatus(403);
-    }
+  try {
+    const decoded = jwt.verify(accessToken, "dmngo");
+    console.log({ decoded });
+    next();
+  } catch (error) {
+    return res.sendStatus(403);
+  }
 }
