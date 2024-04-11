6f356572c701bd776e51c573c6c584e48cc425b8
Yesui <164152047+yesuia09@users.noreply.github.com>
2024-04-11 19:20:24 +0800

    x

--- frontend/src/app/yesui/hailt.tsx
+++ frontend/src/app/yesui/hailt.tsx
@@ -1,6 +1,6 @@
 export function Hailt() {
   return (
-    <div className="flex ml-5 gap-3 items-center bg-white  h-[47px] w-[144px] justify-around rounded shadow-xl">
+    <div className="flex ml-5  items-center bg-white  h-[47px] w-[144px] justify-around rounded shadow-xl">
       <h5 className="text-lime-600">Хайлт</h5>
 
       <h5>/</h5>
