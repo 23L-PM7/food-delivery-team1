94fbbd534151dfa7ff5ba35d4fd36b2ba3cbea4d
dmngo <164151923+DMNgoboki@users.noreply.github.com>
2024-04-11 19:31:36 +0800

    up

--- frontend/src/components/userprofile/UserProfile.tsx
+++ frontend/src/components/userprofile/UserProfile.tsx
@@ -2,7 +2,9 @@
 
 import { useState } from "react";
 import { UserData } from "./Userpro";
-import { Exit, Pencil, Timer } from "../icons/ProfileIcons";
+import { Call, Exit, Mail, Pencil, Timer, Person } from "../icons/ProfileIcons";
+import axios from "axios";
+import { title } from "process";
 
 function myFunction() {
   document.getElementById("myDialog").showModal();
@@ -10,8 +12,32 @@ function myFunction() {
 }
 
 export function UserProfile() {
-  const [edit, setEdit] = useState("");
-  const [Timeout, setTimeout] = useState("");
+  const [edit, setEdit] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+
+
+  const updateUsers = async (id: string) => {
+
+    await axios.put(`http://localhost:9090/users/update/${id}`, {
+      name,
+      email,
+      phoneNumber,
+    })
+      .then(() => {
+        setName("");
+        setEmail("");
+        setPhoneNumber("");
+      });
+    setEdit(false)
+  }
+  // catch (error) {
+  //   console.error("Error:", error);
+  //   alert("wefhwe");
+  //   // alert("There was an error creating a new user.");
+
 
 
   return (
@@ -21,44 +47,51 @@ export function UserProfile() {
           <div className="w-24 rounded-full">
             <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
           </div>
-          <div className="bg-[#FFFFFF] w-[34px] h-[34px] p-2 border rounded-full absolute bottom-0 right-[135px]">
+          <button className="bg-[#FFFFFF] w-[34px] h-[34px] p-2 border rounded-full absolute bottom-0 right-[135px]">
             <Pencil />
-          </div>
+          </button>
         </div>
-
         <h1 className="flex justify-center font-bold text-[28px] mt-[40px]">
           УгтахБаяр
         </h1>
       </div>
 
       <div>
-        {UserData.map((item) => (
-          <div className="bg-[#F6F6F6] w-full rounded px-[20px] py-[8px] flex items-center  gap-[8px] mt-[16px]">
-            <div className="bg-[#FFFFFF] w-[48px] h-[48px] p-3 rounded-full">
-              {item.icon}
-            </div>
-            <div className="w-8/12">
-              <h1 className="text-xs text-[#888A99]">{item.title} </h1>
-              <h1 className="text-base">{item.name}{item.number}{item.email}</h1>
-            </div>
-            <div className="w-2/12 justify-end flex">
-              {item.icon2}
-            </div>
+        <div className="bg-[#F6F6F6] w-full rounded px-[20px] py-[8px] flex items-center  gap-[8px] mt-[40px]">
+          <div className="bg-[#FFFFFF] w-[48px] h-[48px] p-3 rounded-full">
+            <Person />
           </div>
-        ))}
+          <div className="w-8/12">
+            <h1 className="text-xs text-[#888A99]">Таны нэр</h1>
+            <input
+              // placeholder="Нэр..."
+              type="text"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+              className="bg-transition rounded p-2" />
+          </div>
+          <button className="w-2/12 justify-end flex">
+            <Pencil />
+          </button>
+        </div>
 
 
-        {/* <div className="bg-[#F6F6F6] w-full rounded px-[20px] py-[8px] flex items-center  gap-[8px] mt-[16px]">
+        <div className="bg-[#F6F6F6] w-full rounded px-[20px] py-[8px] flex items-center  gap-[8px] mt-[16px]">
           <div className="bg-[#FFFFFF] w-[48px] h-[48px] p-3 rounded-full">
             <Call />
           </div>
           <div className="w-8/12">
             <h1 className="text-xs text-[#888A99]">Утасны дугаар</h1>
-            <h1 className="text-base">88883345</h1>
+            <input
+              // placeholder="Утасны дугаар..."
+              type="text"
+              value={phoneNumber}
+              onChange={(event) => setPhoneNumber(event.target.value)}
+              className="bg-transition rounded p-2" />
           </div>
-          <div className="w-2/12 justify-end flex">
+          <button className="w-2/12 justify-end flex">
             <Pencil />
-          </div>
+          </button>
         </div>
         <div className="bg-[#F6F6F6] w-full rounded px-[20px] py-[8px] flex items-center  gap-[8px] mt-[16px]">
           <div className="bg-[#FFFFFF] w-[48px] h-[48px] p-3 rounded-full">
@@ -66,12 +99,17 @@ export function UserProfile() {
           </div>
           <div className="w-8/12">
             <h1 className="text-xs text-[#888A99]">Имэйл хаяг</h1>
-            <h1 className="text-base">Ugtakhbayr@gmail.com</h1>
+            <input
+              // placeholder="Имэйл хаяг..."
+              type="text"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              className="bg-transition bg-[#F6F6F6]  rounded p-2" />
           </div>
-          <div className="w-2/12 justify-end flex">
+          <button className="w-2/12 justify-end flex" onClick={() => updateUsers}>
             <Pencil />
-          </div>
-        </div> */}
+          </button>
+        </div>
 
         <div className=" w-full rounded px-[20px] py-[8px] flex items-center  gap-[8px] mt-[16px]">
           <div className="bg-[#FFFFFF] w-[48px] h-[48px] p-3 rounded-full border">
