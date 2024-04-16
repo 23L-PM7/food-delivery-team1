f23e7148c70d8d959ced6695d937e645f16ce792
ashrv12 <bshirav@gmail.com>
2024-04-16 17:49:16 +0800

    a

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -8,11 +8,13 @@ import { AdminDashboard } from "@/components/adminComp/adminDashboard";
 import Header from "@/components/Header";
 import { Footer } from "../Footer";
 import { AdminCategory } from "@/components/adminComp/adminCategory";
+import { Category } from "@/components/category";
 
 export default function Home() {
   return (
     <div className="h-dvh">
       <Header />
+      <Category />
       <Footer />
     </div>
   );
