2127809c6be8dd8f3c155f90801a123193dc8c08
ashrv12 <bshirav@gmail.com>
2024-04-24 18:38:19 +0800

    zustand

--- frontend/src/components/foodTemplateMain.tsx
+++ frontend/src/components/foodTemplateMain.tsx
@@ -14,7 +14,7 @@ export function FoodTemplateMain(props: Props) {
   const { item } = props;
 
   // zustand
-  const { cart, addCart } = useCart();
+  const { addCart } = useCart();
   //
 
   const saleCheck = () => {
