ec39c60be14468eb2a1d6fe447b09167d7808885
Yesui <164152047+yesuia09@users.noreply.github.com>
2024-04-25 17:37:56 +0800

    asdasd

--- frontend/src/components/category.tsx
+++ frontend/src/components/category.tsx
@@ -9,38 +9,43 @@ import { create } from "zustand";
 
 export function Category() {
   // const [categories, setCategories] = useState([]);
-  const {category, setCategory}: any = useCategory();
-  const {categories, setCategories}:any = useCategories();
+  const { category, setCategory }: any = useCategory();
+  const { categories, setCategories }: any = useCategories();
 
-  // const fetchCategory = async () => {
-  //   // await axios.get("http://localhost:9090/category").then((response) => {
-  //   //   setCategory(response.data);
-  //   // });
-  //   const data = await utilFetcher("category");
-  //   setCategories(data);
-  //   console.log("mm",data);
-  // };
+  const fetchCategory = async () => {
+    await axios.get("http://localhost:9090/category").then((response) => {
+      setCategory(response.data);
+    });
+    const data = await utilFetcher("category");
+    setCategories(data);
+    console.log("mm", data);
+  };
 
-  // useEffect(() => {
-  //   fetchCategory();
-  // }, []);
+  useEffect(() => {
+    fetchCategory();
+  }, []);
 
   const handleClick = (id: string) => {
-    if(category === id){
-      setCategory('');
-    }
-    else{
+    if (category === id) {
+      setCategory("");
+    } else {
       setCategory(id);
     }
-  }
+  };
 
   return (
     <div className="max-w-[1200px] container mx-auto mt-[32px] mb-[86px]">
       <div className="w-full flex justify-between gap-x-[26px]">
         {categories.map((item: any) => (
-          <button key={item._id} className={`btn w-[280px] h-[43px] outline-slate-200 text-black hover:bg-green-500 hover:text-white ${category === item._id  ? "bg-green-500 text-white" : ''}`} onClick={() => {
-           handleClick(item._id)
-          }}>
+          <button
+            key={item._id}
+            className={`btn w-[280px] h-[43px] outline-slate-200 text-black hover:bg-green-500 hover:text-white ${
+              category === item._id ? "bg-green-500 text-white" : ""
+            }`}
+            onClick={() => {
+              handleClick(item._id);
+            }}
+          >
             {item.name}
           </button>
         ))}
