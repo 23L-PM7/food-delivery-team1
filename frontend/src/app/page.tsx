48181bb0f55aba2d81435fa9283fc5a94d8e43a8
Yesui <164152047+yesuia09@users.noreply.github.com>
2024-04-25 16:48:21 +0800

    update

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -21,9 +21,9 @@ import { FoodTemplateMain } from "@/components/foodTemplateMain";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const {categories, setCategories}: any = useCategories();
-  const {category, setCategory} : any = useCategory();
-  const {foods, setFoods}:any = useFood();
+  const { categories, setCategories }: any = useCategories();
+  const { category, setCategory }: any = useCategory();
+  const { foods, setFoods }: any = useFood();
   const router = useRouter();
 
   const fetchCategory = async () => {
@@ -39,48 +39,50 @@ export default function Home() {
   useEffect(() => {
     fetchCategory();
     fetchFoods();
-  }, [categories, foods]);
+  }, []);
 
   return (
     <div>
       <Header />
-      {/* <UserProfile /> */}
       <Desktop />
       <Mainbutton />
       <HymdraltaiFour />
-      {/* <TraditionalFood /> */}
-      {
-        categories.map((category: any) => {
-          return (
+      {categories.map((category: any) => {
+        return (
           <>
             <div className="max-w-[1200px] mx-auto">
-      <div className="flex flex-col justify-between">
-        <div className="w-full flex justify-between">
-          <div className="flex gap-2">
-            <GreenStar />
-            <h1 className="font-bold">{category.name}</h1>
-          </div>
-          <div className="flex gap-x-2 items-center">
-            <div className="link link-hover text-green-500" onClick={() => {
-              setCategory(category._id);
-              router.push("/menu");
-            }}>
-              Бүгдийг харах
+              <div className="flex flex-col justify-between">
+                <div className="w-full flex justify-between">
+                  <div className="flex gap-2">
+                    <GreenStar />
+                    <h1 className="font-bold">{category.name}</h1>
+                  </div>
+                  <div className="flex gap-x-2 items-center">
+                    <div
+                      className="link link-hover text-green-500"
+                      onClick={() => {
+                        setCategory(category._id);
+                        router.push("/menu");
+                      }}
+                    >
+                      Бүгдийг харах
+                    </div>
+                    <RightArrowMainPage />
+                  </div>
+                </div>
+                <div className="py-10 w-full grid grid-cols-4 gap-6">
+                  {foods
+                    .filter((food: any) => food.categoryId._id === category._id)
+                    .slice(0, 4)
+                    .map((item: any, index: number) => (
+                      <FoodTemplateMain key={1000 - index} item={item} />
+                    ))}
+                </div>
+              </div>
             </div>
-            <RightArrowMainPage />
-          </div>
-        </div>
-        <div className="py-10 w-full grid grid-cols-4 gap-6">
-          {foods.filter((food: any) => food.categoryId._id === category._id).slice(0, 4).map((item: any, index: number) => (
-            <FoodTemplateMain key={1000 - index} item={item} />
-          ))}
-        </div>
-      </div>
-    </div>
           </>
-        )
-        })
-      }
+        );
+      })}
       <Footer />
     </div>
   );
