fa436717d7bb6bdecbb4e6b22713ba50fd4f4884
ashrv12 <bshirav@gmail.com>
2024-04-26 19:34:04 +0800

    update

---
+++ frontend/src/app/admin/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header";
+import { Footer } from "../../components/footer/Footer";
+import { AdminDashboard } from "@/components/adminComp/adminDashboard";
+
+export default function Admin() {
+  return (
+    <div>
+      <Header />
+      <AdminDashboard />
+    </div>
+  );
+}
